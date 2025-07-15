6a116bc91fc62d7fdd37c98be2798308fc85ba6d
Duncan Farthing <duncan@theoblong.com>
2025-07-15 14:08:30 +0100

    login implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello, {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
 
-export default App
+export default App;
